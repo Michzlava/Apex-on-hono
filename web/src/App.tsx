a6a43785f4cac32a52d8d270978eaa2ca9dc0dc3
Michzlava <michelle_zavala211@mail.com>
2026-08-19 12:47:02 +0100

    Add SignupPage route to App component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
+import SignupPage from './pages/SignupPage' // <-- ADDED
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      {/* Add other routes like /login, /dashboard later */}
+      <Route path="/signup" element={<SignupPage />} /> {/* <-- ADDED */}
+      {/* We will add /login and /dashboard next */}
     </Routes>
   )
 }
