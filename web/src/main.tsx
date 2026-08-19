84312af054768bbc67d24231eea6be7dc5b181f6
Michzlava <michelle_zavala211@mail.com>
2026-08-19 20:29:46 +0100

    Implement debug mode with eruda integration
    Add conditional debug mode to load eruda for debugging.

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,6 +4,12 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+if (new URLSearchParams(window.location.search).get('debug') === 'true') {
+  import('eruda').then(({ default: eruda }) => {
+    eruda.init()
+  })
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
