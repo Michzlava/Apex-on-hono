1ba34d1e4db785b84bbdc99fb4c572ba752d9da2
Michzlava <michelle_zavala211@mail.com>
2026-08-20 19:54:29 +0100

    Fix import statement for Logo component

--- web/src/pages/LoginPage.tsx
+++ web/src/pages/LoginPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Logo } from "../components/Logo";
+import   Logo  from "../components/Logo";
 import { useAuth } from "../context/AuthContext";
 import "./login.css";
 
