010e0ea0f16b8117f77c6727ce30925c6e9d388a
Michzlava <michelle_zavala211@mail.com>
2026-08-20 19:56:58 +0100

    Update DashboardLayout.tsx

--- web/src/components/DashboardLayout.tsx
+++ web/src/components/DashboardLayout.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link, useLocation, Outlet } from 'react-router-dom';
-import { Logo } from './Logo';
+import   Logo  from './Logo';
 import { useAuth } from '../context/AuthContext';
 import '../pages/dashboard.css';
 
